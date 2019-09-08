4e209f2b07c983d8071bf59a793559fbcb39e638
Prabhu S <s.prabhuvijay@gmail.com>
2019-09-08 19:42:53 +0200

    Formatted the code

--- src/main/java/com/mcoder/sample/app/ReverseString.java
+++ src/main/java/com/mcoder/sample/app/ReverseString.java
@@ -6,40 +6,35 @@ import java.util.Objects;
  * Reverse a given String!
  *
  */
-public class ReverseString 
-{
-    public static void main( String[] args )
-    {
-        try {
-        	reverseString(args[0]);
-        }
-        catch(ArrayIndexOutOfBoundsException
-        		e) {
-        	System.out.println("Empty String can't be reversed");
-        }
-    }
+public class ReverseString {
+	public static void main(String[] args) {
+		try {
+			reverseString(args[0]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Empty String can't be reversed");
+		}
+	}
 
 	public static String reverseString(String given) {
-		
-		String givenString  = given;
-		
+
+		String givenString = given;
+
 		if (Objects.isNull(given)) {
 			System.out.println("Empty String can't be reversed");
 			return "";
 		}
-    	
-    	System.out.println("Given String    ******   " + givenString);
-    	String reversed = "";
-    	String tempArray[] = givenString.split("");
-    	
-    	for(String c : tempArray) {
-    		reversed = c + reversed;
-    	}
-    	
-    	System.out.println("Reversed String ******   " + reversed);
-    	
-    	return reversed;
+
+		System.out.println("Given String    ******   " + givenString);
+		String reversed = "";
+		String tempArray[] = givenString.split("");
+
+		for (String c : tempArray) {
+			reversed = c + reversed;
+		}
+
+		System.out.println("Reversed String ******   " + reversed);
+
+		return reversed;
 	}
-    
-    
+
 }
